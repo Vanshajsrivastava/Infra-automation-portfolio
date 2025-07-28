f00d7979d4bc09e5e209cfa5e7cad44a919610a9
Vanshaj Srivastava <vanshaj@vanshaj.localdomain>
2025-07-28 16:21:14 +0100

    main.tf

--- terraform-vpc-ec2/main.tf
+++ terraform-vpc-ec2/main.tf
@@ -7,17 +7,44 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet_cidr
-  availability_zone = "${var.aws_region}a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "MyTerraformSubnet"
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "MyIGW"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0b6ab5dd5da28b19a" # Example Amazon Linux 2 AMI for eu-west-2
+  ami           = "ami-0b6ab5dd5da28b19a"
   instance_type = var.instance_type
   subnet_id     = aws_subnet.main.id
 
--- terraform-vpc-ec2/outputs.tf
+++ terraform-vpc-ec2/outputs.tf
@@ -17,3 +17,8 @@ output "instance_private_ip" {
   description = "The private IP address of the EC2 instance"
   value       = aws_instance.example.private_ip
 }
+
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
