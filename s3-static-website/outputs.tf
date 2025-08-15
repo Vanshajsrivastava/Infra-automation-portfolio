003a072681ed1111cd092b454b128f6da5b695e0
Vanshaj <srivastavanshaj10j@gmail.com>
2025-08-15 04:19:52 +0100

    Fix: declassify bucket_name output for TFC

--- s3-static-website/outputs.tf
+++ s3-static-website/outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
   description = "S3 bucket name"
-  value       = aws_s3_bucket.site.bucket
-}
-
-output "cloudfront_domain" {
-  description = "CloudFront HTTPS URL"
-  value       = aws_cloudfront_distribution.cdn.domain_name
+  value       = nonsensitive(aws_s3_bucket.site.bucket)
 }
 
 output "cloudfront_distribution_id" {
-  description = "ID for invalidations / status checks"
+  description = "ID for CloudFront invalidations"
   value       = aws_cloudfront_distribution.cdn.id
+}
+
+output "cloudfront_domain" {
+  description = "CloudFront domain"
+  value       = aws_cloudfront_distribution.cdn.domain_name
 }
